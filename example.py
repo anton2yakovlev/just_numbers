a0b4a4e4353853b4ff1dcddd22797a58cc0b82a7
Антон Яковлев <anton2yakovlev@gmail.com>
2022-02-04 02:11:05 +0300

    style fix

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
 from Numbers import Five
 
 five = Five()
-print(five) # 5
-print(five == 5) # True
-print(abs(five)) # 5
-print(five ** 3) # 125
-print(hash(five)) # 5
+print(five)  # 5
+print(five == 5)  # True
+print(abs(five))  # 5
+print(five ** 3)  # 125
+print(hash(five))  # 5
